0e5882274e7dda624ef1251ac112066659bcce4c
Praneshk12 <77246096+Praneshk12@users.noreply.github.com>
2021-09-06 10:53:56 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,12 @@ import firebase from 'firebase'
 require("@firebase/firestore")
 
 const firebaseConfig = {
- //Add configuration here
- 
+  apiKey: "AIzaSyDjGgiVpOh9S3oXj7osalOLp02bU-lzUPk",
+  authDomain: "pro-c73---let-s-ride.firebaseapp.com",
+  projectId: "pro-c73---let-s-ride",
+  storageBucket: "pro-c73---let-s-ride.appspot.com",
+  messagingSenderId: "637225328193",
+  appId: "1:637225328193:web:71f98f982c4f92706530c0"
 };
   firebase.initializeApp(firebaseConfig);
 
